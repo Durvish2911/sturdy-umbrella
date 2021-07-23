98a698be6d559c8bb0e1bf8dd82561b2e836dbeb
Durvish2911 <72204035+Durvish2911@users.noreply.github.com>
2021-07-23 20:25:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+function draw(){
+
+}
+function takesnap(){
+    save("the michty ahoms who defeted mughals 17 times.jpeg")
+}
